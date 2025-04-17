1f7a83036839873fafd248f521c3e7b66a767cb0
Gabriel Molina <gabrielmolina717@gmail.com>
2025-04-17 13:50:30 +0800

    edited menu.js

--- src/menu-module/menu.js
+++ src/menu-module/menu.js
@@ -4,6 +4,33 @@ import beefImage from "./menu-assets/beef.jpg";
 import bicolImage from "./menu-assets/bicol.jpg";
 import mangoImage from "./menu-assets/mango.jpg";
 
+const menuObjects = [
+  {
+    image: adoboImage,
+    name: "Spicy Chicken Adobo",
+    description:
+      "A bold twist on a Filipino classic—tender chicken braised in soy, vinegar, and spices, finished with a touch of heat for a rich, warming depth of flavor",
+  },
+  {
+    image: beefImage,
+    name: "Beef Brocolli",
+    description:
+      "Sliced tender beef sautéed with crisp broccoli in a savory garlic-soy glaze—an elegant harmony of texture and flavor with a Filipino touch",
+  },
+  {
+    image: bicolImage,
+    name: "Bicol Express",
+    description:
+      "Pork simmered in creamy coconut milk with chilies and shrimp paste—a flavorful dish rooted in Bicolano tradition, with just the right balance of spice and richness",
+  },
+  {
+    image: mangoImage,
+    name: "Mango Float",
+    description:
+      "Layers of ripe mangoes, whipped cream, and graham crackers chilled to perfection—a light, creamy dessert that’s both refreshing and indulgent",
+  },
+];
+
 export default function generateMenu() {
   const menuPageDiv = document.createElement("div");
   menuPageDiv.classList.add("menu-page");
@@ -11,20 +38,16 @@ export default function generateMenu() {
   const menuContainerDiv = document.createElement("div");
   menuContainerDiv.classList.add("menu-container");
 
-  const menuObjects = [
-    { image: adoboImage, name: "adobo", description: "adobo good" },
-    { image: beefImage, name: "beef brocolli", description: "beef good" },
-    { image: bicolImage, name: "bicol express", description: "spicy good" },
-    { image: mangoImage, name: "mango float", description: "mango good" },
-  ];
-
   for (let i = 0; i < menuObjects.length; i++) {
     const menuCardDiv = document.createElement("div");
     menuCardDiv.classList.add("menu-card");
 
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+
     const img = document.createElement("img");
-    img.setAttribute("width", "200px");
-    img.setAttribute("height", "250px");
+    img.setAttribute("width", "250");
+    img.setAttribute("height", "300px");
     img.src = menuObjects[i].image;
 
     const headerThree = document.createElement("h3");
@@ -33,9 +56,11 @@ export default function generateMenu() {
     const descriptionParagraph = document.createElement("p");
     descriptionParagraph.textContent = menuObjects[i].description;
 
+    textContainer.appendChild(headerThree);
+    textContainer.appendChild(descriptionParagraph);
+
     menuCardDiv.appendChild(img);
-    menuCardDiv.appendChild(headerThree);
-    menuCardDiv.appendChild(descriptionParagraph);
+    menuCardDiv.appendChild(textContainer);
 
     menuContainerDiv.appendChild(menuCardDiv);
   }
